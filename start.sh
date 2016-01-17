12a614bde16738b3a494047ad0cb17c012f4dda2
Lars Kluge <l@larskluge.com>
2016-01-18 00:14:40 +0100

    Print when initial config runs

--- start.sh
+++ start.sh
@@ -7,6 +7,7 @@ chown -R git /home/git/.ssh
 chmod -R 0700 /home/git/.ssh
 
 if [ ! -f /home/git/repositories/.gitolite-configured ]; then
+  echo Initial configuration of gitolite
   echo $SSH_KEY > /tmp/admin.pub
   su git -c "/home/git/bin/gitolite setup -pk=/tmp/admin.pub"
   su git -c "touch /home/git/repositories/.gitolite-configured"
