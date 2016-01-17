d38ff22700b479af26fd199438e81853e5cdfab5
Lars Kluge <l@larskluge.com>
2016-01-17 23:37:51 +0100

    Simplify start.sh for my use-case

--- start.sh
+++ start.sh
@@ -3,30 +3,11 @@
 chown -R git /home/git/repositories
 chmod -R u+rwX /home/git/repositories
 
-# handle the gitolite.rc
-if [  -f "/home/git/repositories/gitolite.rc" ]; then
-  echo 'import rc file'
-  su git -c "cp /home/git/repositories/gitolite.rc /home/git/.gitolite.rc"
-else
-  echo 'export rc file'
-  su git -c "cp /home/git/.gitolite.rc /home/git/repositories/gitolite.rc"
-fi
+echo $SSH_KEY > /tmp/admin.pub
 
-if [ -f /home/git/repositories/gitolite-configured ]; then
-  su git -c "/home/git/bin/gitolite setup"
-else
-  # handle the ssh key
-  if [ -n "$SSH_KEY" ]; then
-    echo "Replace the admin ssh key.\n"
-    echo $SSH_KEY > /home/git/admin.pub
-    su git -c "/home/git/bin/gitolite setup -pk=/home/git/admin.pub"
-  else
-    su git -c "/home/git/bin/gitolite setup"
-    echo "The built-in private key for admin:\n"
-    cat /admin
-  fi
-
-  su git -c "touch /home/git/repositories/gitolite-configured"
+if [ ! -f /home/git/repositories/.gitolite-configured ]; then
+  su git -c "/home/git/bin/gitolite setup -pk=/tmp/admin.pub"
+  su git -c "touch /home/git/repositories/.gitolite-configured"
 fi
 
 /usr/sbin/sshd -D
