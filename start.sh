7b9ee58626eecc1ad6ede15951691d9d520c2647
Lars Kluge <l@larskluge.com>
2016-01-17 23:57:50 +0100

    Pull more state out of the container

--- start.sh
+++ start.sh
@@ -3,9 +3,11 @@
 chown -R git /home/git/repositories
 chmod -R u+rwX /home/git/repositories
 
-echo $SSH_KEY > /tmp/admin.pub
+chown -R git /home/git/.ssh/authorized_keys
+chmod -R 0600 /home/git/.ssh/authorized_keys
 
 if [ ! -f /home/git/repositories/.gitolite-configured ]; then
+  echo $SSH_KEY > /tmp/admin.pub
   su git -c "/home/git/bin/gitolite setup -pk=/tmp/admin.pub"
   su git -c "touch /home/git/repositories/.gitolite-configured"
 fi
