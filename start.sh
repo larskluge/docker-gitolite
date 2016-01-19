c7d6cb1f79cf0dc31dcac84cbbeb0514f5d75c96
Lars Kluge <l@larskluge.com>
2016-01-19 19:02:29 +0100

    Backup gitolite-admin repo before calling setup, then push it back afterwards..

--- start.sh
+++ start.sh
@@ -1,16 +1,35 @@
-#! /bin/sh -
+#! /bin/bash -x
 
-chown -R git /home/git/repositories
-chmod -R u+rwX /home/git/repositories
+reset_admin_repo=false
+git config --global push.default simple
 
-chown -R git:git /home/git/.ssh
-chmod -R u+rwX /home/git/.ssh
+cd /home/git
 
-if [ ! -f /home/git/repositories/.gitolite-configured ]; then
-  echo Initial configuration of gitolite
-  echo $SSH_KEY > /tmp/admin.pub
-  su git -c "/home/git/bin/gitolite setup -pk=/tmp/admin.pub"
-  su git -c "touch /home/git/repositories/.gitolite-configured"
+echo Fix file permissions
+chown -R git repositories
+chmod -R u+rwX repositories
+chown -R git:git .ssh
+chmod -R u+rwX .ssh
+
+cd /home/git
+if [ -d "repositories/gitolite-admin.git" ]; then
+  git clone repositories/gitolite-admin.git /tmp/gitolite-admin
+  cd /tmp/gitolite-admin
+  commit_cnt=$(git rev-list HEAD --count)
+  if [ "$commit_cnt" -gt "1" ]; then
+    reset_admin_repo=true
+  fi
 fi
+
+echo $SSH_KEY > /tmp/admin.pub
+cd /home/git
+su git -c "bin/gitolite setup -pk=/tmp/admin.pub"
+
+if [ "$reset_admin_repo" = true ]; then
+  cd /tmp/gitolite-admin
+  git push --force
+fi
+
+rm -rf /tmp/gitolite-admin
 
 /usr/sbin/sshd -D
